c67b829be5cd5371e67fe4a49614fa94b56d1521
Antonio Peres <idevantonio@gmail.com>
2025-09-03 11:36:20 -0300

    Add cancel button to FormPage and update Button component to accept type prop

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,5 @@
-import { InputText, Template } from "@/components"
+import { InputText, Template, Button } from "@/components"
+import Link from "next/link"
 
 export default function FormPage() {
   return (
@@ -22,11 +23,11 @@ export default function FormPage() {
                     <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                         <div className="text-center">
                             <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 
+                                <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 
                                 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 
                                 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 
                                 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 
-                                1.125 0 01-2.25 0z" clip-rule="evenodd"></path>
+                                1.125 0 01-2.25 0z" clipRule="evenodd"></path>
                             </svg>
                             <div className="mt-4 flex text-sm leading-6 text-gray-600">
                                 <label htmlFor="file-upload" className="relative cursor-pointer rrounded-md bg-white font-semibold text-indigo-600">
@@ -36,6 +37,12 @@ export default function FormPage() {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="mt-6 flex justify-end gap-x-6">
+                    <Link href={'/gallery'}>
+                        <Button style="bg-gray-600 hover:bg-gray-700" type="button" label="Cancel"/>
+                    </Link>
+                    <Button style="bg-blue-500 hover:bg-blue-600" type="submit" label="Submit"/>
                 </div>
             </form>
         </section>
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,13 +4,15 @@ interface ButtonProps {
     style?:  string;
     label?: string;
     onClick?: (event: any) => void;
+    type?: 'button' | 'submit' | 'reset' | undefined;
     
 }
 
-export const Button: React.FC<ButtonProps> = ({onClick,style,label}: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({onClick,style,label, type}: ButtonProps) => {
     return(
         <>
             <button onClick={onClick} 
+            type={type}
             className={`${style} text-white px-4 py-2 rounded-lg hover: transition-colors duration-300`}>
                 {label}
             </button>
